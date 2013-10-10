6747179adafc6206b5f915a066bca6d6941835b6
adlawson <adlawson@gmail.com>
2013-10-10 15:33:55 +0100

    Update test bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,4 @@
 <?php
 
 $loader = require dirname(__DIR__) . '/vendor/autoload.php';
-$loader->add('Graze\\Monolog\\Test', __DIR__ . '/lib');
 $loader->add('Monolog', dirname(__DIR__) . '/vendor/monolog/monolog/tests');
