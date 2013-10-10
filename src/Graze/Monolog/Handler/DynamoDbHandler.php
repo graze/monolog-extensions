a694cb727b7fca1a14cf27efbc076dffb805f78d
adlawson <adlawson@gmail.com>
2013-10-10 16:04:34 +0100

    Add license information to source

--- src/Graze/Monolog/Handler/DynamoDbHandler.php
+++ src/Graze/Monolog/Handler/DynamoDbHandler.php
@@ -1,4 +1,15 @@
 <?php
+/*
+ * This file is part of Monolog Extensions
+ *
+ * Copyright (c) 2013 Nature Delivered Ltd. <http://graze.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @see  http://github.com/graze/MonologExtensions/blob/master/LICENSE
+ * @link http://github.com/graze/MonologExtensions
+ */
 namespace Graze\Monolog\Handler;
 
 use Aws\DynamoDb\DynamoDbClient;
