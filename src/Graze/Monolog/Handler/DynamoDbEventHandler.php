e47edc3ccecc2efa31501e0c1eaf2a01e54ec1e5
Harry Bragg <harry.bragg@graze.com>
2018-06-20 09:15:45 +0100

    handle whoops with php7

--- src/Graze/Monolog/Handler/DynamoDbEventHandler.php
+++ src/Graze/Monolog/Handler/DynamoDbEventHandler.php
@@ -14,6 +14,9 @@ namespace Graze\Monolog\Handler;
 
 use Monolog\Handler\DynamoDbHandler;
 
+/**
+ * @deprecated This add no functionality over the Monolog DynamoDbHandler
+ */
 class DynamoDbEventHandler extends DynamoDbHandler
 {
     /**
--- tests/unit/src/Graze/Monolog/Handler/WhoopsHandlerTest.php
+++ tests/unit/src/Graze/Monolog/Handler/WhoopsHandlerTest.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Graze\Monolog\Handler;
 
 use Mockery as m;
@@ -11,7 +12,7 @@ class WhoopsHandlerTest extends TestCase
         if (!interface_exists('Whoops\Handler\HandlerInterface', true)) {
             $this->markTestSkipped('filp/whoops not installed');
         }
-        
+
         $this->handlerWhoops = m::mock('Whoops\Handler\HandlerInterface');
     }
 
@@ -28,62 +29,111 @@ class WhoopsHandlerTest extends TestCase
     public function testHandleError()
     {
         $record = $this->getRecord(300, 'test', ['file' => 'bar', 'line' => 1]);
-        
-        $formatter = m::mock('Monolog\\Formatter\\FormatterInterface');
-        
         $handlerMonolog = new WhoopsHandler($this->handlerWhoops);
-             
+
         $this->handlerWhoops
             ->shouldReceive('setInspector')
             ->once()
             ->with(m::type('Whoops\Exception\Inspector'));
-              
+
         $this->handlerWhoops
             ->shouldReceive('setRun')
             ->once()
             ->with(m::type('Whoops\Run'));
-               
+
         $this->handlerWhoops
             ->shouldReceive('setException')
             ->once()
             ->with(m::type('Whoops\Exception\ErrorException'));
-             
+
         $this->handlerWhoops
             ->shouldReceive('handle')
             ->once();
-             
+
         $handlerMonolog->handle($record);
     }
 
     public function testHandleException()
     {
         $exception = new \Whoops\Exception\ErrorException('foo');
-        
         $record = $this->getRecord(300, 'foo', ['exception' => $exception]);
-        
-        $formatter = m::mock('Monolog\\Formatter\\FormatterInterface');
-        
         $handlerMonolog = new WhoopsHandler($this->handlerWhoops);
-             
+
         $this->handlerWhoops
             ->shouldReceive('setInspector')
             ->once()
             ->with(m::type('Whoops\Exception\Inspector'));
-              
+
         $this->handlerWhoops
             ->shouldReceive('setRun')
             ->once()
             ->with(m::type('Whoops\Run'));
-               
+
         $this->handlerWhoops
             ->shouldReceive('setException')
             ->once()
             ->with($exception);
-             
+
         $this->handlerWhoops
             ->shouldReceive('handle')
             ->once();
-             
+
         $handlerMonolog->handle($record);
+    }
+
+    /**
+     * @requires PHP 7
+     */
+    public function testHandleThrowable()
+    {
+        $exception = new \TypeError('foo');
+        $record = $this->getRecord(300, 'foo', ['exception' => $exception]);
+        $handlerMonolog = new WhoopsHandler($this->handlerWhoops);
+
+        $this->handlerWhoops
+            ->shouldReceive('setInspector')
+            ->once()
+            ->with(m::type('Whoops\Exception\Inspector'));
+
+        $this->handlerWhoops
+            ->shouldReceive('setRun')
+            ->once()
+            ->with(m::type('Whoops\Run'));
+
+        $this->handlerWhoops
+            ->shouldReceive('setException')
+            ->once()
+            ->with($exception);
+
+        $this->handlerWhoops
+            ->shouldReceive('handle')
+            ->once();
+
+        $handlerMonolog->handle($record);
+    }
+
+    /**
+     * @dataProvider nonTriggeringData
+     *
+     * @param array $context
+     */
+    public function testContextWithNotEnoughInformationDoesNotTrigger(array $context)
+    {
+        $record = $this->getRecord(300, 'test', $context);
+        $handlerMonolog = new WhoopsHandler($this->handlerWhoops);
+        $handlerMonolog->handle($record);
+    }
+
+    /**
+     * @return array
+     */
+    public function nonTriggeringData()
+    {
+        return [
+            [['file' => 'file']],
+            [['line' => 1]],
+            [[]],
+            [['other' => 5]],
+        ];
     }
 }
